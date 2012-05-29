4b6d2df865df8c9ea5afa34392af26ed3dac3db8
Anonymous <anonymous@anonymous.com>
2012-05-29 18:15:45 +0600

    Removed overridden finalize_update task. Now capistrano (2.12) may share directory out the box.

--- lib/capistrano/phptasks/base.rb
+++ lib/capistrano/phptasks/base.rb
@@ -20,24 +20,6 @@ configuration.load do
         task :restart, :roles => :app do
             # no restart required for Apache/mod_php
         end
-
-        # Override internal task
-        task :finalize_update, :except => { :no_release => true } do
-            run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-        	
-            # mkdir -p is making sure that the directories are there for some SCM's that don't
-            # save empty folders
-            run <<-CMD
-                rm -rf #{latest_release}/logs &&
-                ln -s #{shared_path}/logs #{latest_release}/logs
-            CMD
-        
-            #if fetch(:normalize_asset_timestamps, true)
-            #  stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-            #  asset_paths = %w(images css js).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
-            #  run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
-            #end
-        end
         
         # Write :branch variable contents into VERSION file to identify deployed revision
         task :write_version do
